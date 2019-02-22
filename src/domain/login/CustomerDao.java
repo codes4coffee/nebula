7eb56da7f99a9ec19abacd3be28f8b9fe7efc780
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-02-22 00:33:20 -0600

    Format Mehra's Java code

--- src/domain/login/CustomerDao.java
+++ src/domain/login/CustomerDao.java
@@ -1,26 +1,17 @@
 package domain.login;
+
 /**
- * 
  * @author mehra
- * The methods that we need to save and retrieve data from the database
+ * The methods that we need to save and retrieve data from the database.
  */
 public interface CustomerDao {
-
+	int register(Customer c);
+	
 	/**
-	 * 
-	 * @param c
-	 * @return
+	 * Retrieve the Customer object from the database.
 	 */
-	public int register(Customer c);
-	
-	/*
-	 * Retrieve the Customer object from the database
-	 */
-	public Customer validateCustomer(Login login);
+	Customer validateCustomer(Login login);
 
-	
-	//public Customer getCustomer(String username, String pass); This method does not needed as we have the Login object
-
-	
+	// This method is not needed as we have the Login object.
+	//Customer getCustomer(String username, String pass);
 }
-
--- src/domain/login/Login.java
+++ src/domain/login/Login.java
@@ -1,29 +1,31 @@
 package domain.login;
 
 /**
- * 
  * @author mehra
  * This is the Customer before the validation.
  */
 public class Login {
-	private String username;
-	private String password;
-	
-	public Login(String username, String pass){
-		this.username = username;
-		this.password = pass;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    private String username;
+    private String password;
 
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
